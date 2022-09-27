a457aa10e418ded938c7e84699156f88d86021af
Luvie Rallos <luvierallos@gmail.com>
2022-09-27 15:52:58 +0800

    Add correct answers to exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...other) {
+    other.forEach(number => {
+        array.forEach((arrayNumber, index) =>{
+            if (arrayNumber === number) {
+                array.splice(index, 1)
+            }
+        })
+    })
+    return array
+}   
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    result = 0;
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    } 
+    if (typeof x !== "number" || typeof y !== "number"){
+        return "ERROR";
+    }
 
+    if (x < y){
+        for (i = x; i < (y+1); i++) {
+            result += i;
+            
+        };
+        return result;
+    } else if (x > y){
+        for (i = y; i < (x+1); i++) {
+            result += i;
+            
+        };
+        return result;
+    }
+
+    
 };
 
 // Do not edit below this line
